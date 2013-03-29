df380f34980958daac0f1bb8c4d81074f3b650e4
Vitaliy Lebedev <malgin05@gmail.com>
2013-03-29 05:07:30 +0200

    Add csrf protection to admin login form

--- src/LinkZone/Core/AdminBundle/Controller/SecurityController.php
+++ src/LinkZone/Core/AdminBundle/Controller/SecurityController.php
@@ -22,12 +22,17 @@ class SecurityController extends Controller
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         }
 
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
         return $this->render(
             'LinkZoneCoreAdminBundle:Security:login.html.twig',
             array(
                 // last username entered by the user
                 'last_username' => $session->get(SecurityContext::LAST_USERNAME),
                 'error'         => $error,
+                'csrf_token' => $csrfToken,
             )
         );
     }
