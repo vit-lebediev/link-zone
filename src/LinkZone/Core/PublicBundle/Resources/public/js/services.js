fa29d513a9a6318fc77cbcd89cb20ef54b52c466
Vitaliy Lebedev <vlebedev@dataart.com>
2013-07-31 23:18:14 +0300

    Integrate AngularJS into platform search page, part of #39

--- src/LinkZone/Core/PublicBundle/Resources/public/js/services.js
+++ src/LinkZone/Core/PublicBundle/Resources/public/js/services.js
@@ -3,6 +3,7 @@ console.log("From services");
 angular.module('publicServices', ['ngResource']).
     factory('Platform', function($resource){
         return $resource('/app_dev.php/api/platforms/:platformId.json', {}, {
-            query: {method: 'GET', params: {platformId: 'all'}, isArray: true}
+            query: {method: 'GET', params: {platformId: 'all'}, isArray: true},
+            search: {method: 'GET', params: {platformId: 'search'}, isArray: true}
         });
     });
